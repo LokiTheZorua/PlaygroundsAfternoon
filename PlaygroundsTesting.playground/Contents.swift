2d809412c3702e2e1df1242eed801e7989014469
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-25 13:28:51 -0600

    added String change and text compare

--- PlaygroundsTesting.playground/Contents.swift
+++ PlaygroundsTesting.playground/Contents.swift
@@ -6,8 +6,19 @@ var myName :String
 var otherName = "Words"
 var thisName :String = "meow"
 
+var math :String
+math = "4"
+math = "10"
+
 var adder = 3
 
 let mess = "\(adder) plus 14 equals \(Double(adder)+14))"
 
-let thisMessage = "This sentance has XX letters, which isn't a lot"
+let thisMessage = "This sentance has 47 letters, which isn't a lot"
+
+print ("ThisMessage has \(thisMessage.characters.count) Characters")
+
+let text1 = "Hi"
+let text2 = "HI"
+
+print(text1 == text2)
