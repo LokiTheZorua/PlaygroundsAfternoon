fb07318bc91530d915080f47b0b47a5361bd25fc
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-25 13:05:24 -0600

    added more string elements

--- PlaygroundsTesting.playground/Contents.swift
+++ PlaygroundsTesting.playground/Contents.swift
@@ -5,3 +5,9 @@ import UIKit
 var myName :String
 var otherName = "Words"
 var thisName :String = "meow"
+
+var adder = 3
+s
+let mess = "\(adder) plus 14 equals \(Double(adder)+14))"
+
+let thisMessage = "This sentance has XX letters, which isn't a lot"
