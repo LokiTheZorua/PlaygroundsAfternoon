718fc15c9b79d238b056978398ceff7a2e9d12a5
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-28 12:42:01 -0600

    added text

--- methodsifsandwhiles.playground/Contents.swift
+++ methodsifsandwhiles.playground/Contents.swift
@@ -20,3 +20,30 @@ let words = "Marshal Mathers"
 aBitLessSimple(name: words)
 
 public func namingExample(outerName innerName :String) ->Void
+{
+    print("The inner name is: \(innerName)")
+}
+namingExample(outerName: "Cody")
+
+public func count() -> Int
+{
+    return (7 *665)
+}
+print ("I am not \(count) years old")
+
+if(count() < 10)
+{
+    print("math works in swift too")
+}
+else
+{
+    print("not very likely now")
+}
+var test = 0
+while (test < count())
+{
+    print("hahahahhahahahahahhahahahahhahaahhaha")
+    test += 1
+}
+
+print("yay!")
