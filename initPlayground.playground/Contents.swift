c4b01d75613a849a77afab5d15f64c6844d2ac8d
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-10-02 12:24:55 -0600

    added ojectsplayground and added an object

---
+++ initPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var notesToday = "Using swift to mak classes and methods"
+
+public class SimpleClass
+{
+    private var name :String
+    
+    public init()
+    {
+        name = "My name is....."
+    }
+}
